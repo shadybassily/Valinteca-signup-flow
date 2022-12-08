ccfd05caaf0ac89ce9e6f8e245fb829ee114a739
shady bassily <shadybassily@yahoo.com>
2022-12-08 16:45:34 +0200

    added show password feature

--- src/components/form/FormInput.jsx
+++ src/components/form/FormInput.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import ErrorMessage from "../error/ErrorMessage";
+import { AiOutlineEye } from "react-icons/ai";
 
 export default function FormInput({
   icon,
@@ -9,16 +10,30 @@ export default function FormInput({
   errorMessage,
   handlePasswordInput,
 }) {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <div>
       <div className={`input-container ${errorMessage && "error-border"}`}>
         <span className="icon-holder">{icon}</span>
         <input
-          type={type}
+          type={isVisible ? "text" : type}
           placeholder={placeHolder}
           {...register}
           onInput={handlePasswordInput}
         />
+        {type === "password" && (
+          <span
+            onMouseEnter={(e) => {
+              setIsVisible(true);
+            }}
+            onMouseLeave={(e) => {
+              setIsVisible(false);
+            }}
+          >
+            <AiOutlineEye />
+          </span>
+        )}
       </div>
       <ErrorMessage message={errorMessage} />
     </div>
